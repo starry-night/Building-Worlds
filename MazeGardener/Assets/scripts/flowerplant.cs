ed5f1f374fb4d80fe3260eb50bdd217cac637ba1
star1117 <jianh550@newschool.edu>
2014-10-31 03:24:03 -0400

    Revert "MazeGarderner"
    This reverts commit d25554fbccba29967608d9d0b41ffcec468cc49c.

--- MazeGardener/Assets/scripts/flowerplant.cs
+++ MazeGardener/Assets/scripts/flowerplant.cs
@@ -18,7 +18,7 @@ public class flowerplant : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Time.time > nextPlantingTime ) {
-			if (Random.Range (0f, 1f) > 0.4f ) {
+			if (Random.Range (0f, 1f) > 0.3f ) {
 				Instantiate ( peashooter, transform.position, Quaternion.identity );
 				nextPlantingTime += Random.Range( plantInterval, plantInterval+5f);
 			}
